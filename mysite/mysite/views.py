e444a9bd8940530ae936b33b33dea9e054d3e56e
aarshsaxena <iaarsh89@gmail.com>
2023-11-25 21:30:15 +0530

    register function

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -11,7 +11,44 @@ def home(request):
     return render(request,"index.html")
 
 def signup(request):
-    return render (request,"signup.html")
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('/home/')
+
+    else:
+        if request.method == "POST": # To register users.
+            try:
+                fname = request.POST.get('fname')
+                lname = request.POST.get('lname')
+                city = request.POST.get('city')
+                mob = request.POST.get('mob')
+                email = request.POST.get('email')
+                cemail = request.POST.get('cemail')
+                passwd = request.POST.get('passwd')
+                cpasswd = request.POST.get('cpasswd')
+
+
+                if email != cemail:
+                    mstr1 = 'Email and Confirm Email are not same.'
+                    messages.error(request, mstr1)
+                    return HttpResponseRedirect('/register/')
+                if passwd != passwd:
+                    mstr2 = 'Email and Confirm Email are not same.'
+                    messages.error(request, mstr2)
+                    return HttpResponseRedirect('/register/')
+
+                user = User.objects.create_user(username=cemail, email=cemail, password=cpasswd)
+                user.first_name = fname
+                user.last_name = lname
+                user.save()
+                mstr3 = 'You account has been successfully created.'
+                messages.success(request, mstr3)
+                return HttpResponseRedirect("/login/")
+
+            except Exception as e:
+                messages.error(request, e)
+                return HttpResponseRedirect('/register/')
+        
+        return render(request, "register.html")
 
 def login(request):
 
@@ -24,7 +61,7 @@ def login(request):
             passwd1 = request.POST['psw']
 
             user = authenticate(request, username=email1, password=passwd1)
-            
+
             if user is not None:
                 auth_login(request, user)
                 return HttpResponseRedirect('/home/')
